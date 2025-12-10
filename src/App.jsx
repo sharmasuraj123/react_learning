1c7a279cded5e74eefed7d7bfee0f8dbccfa1891
Apple <apple@Apples-MacBook-Air.local>
2025-12-10 23:03:28 +0530

    second commit and learning of useEffect and fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,90 @@
-import { useState } from "react";
-import { PostComponent } from "./postComponent";
+// import { useEffect, useState } from "react";
+
+// function App() {
+//   const [currentTab, setCurrentTab] = useState(1);
+//   const [tabData, setTabData] = useState({});
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(function() {
+//     setLoading(true);
+//     fetch("https://jsonplaceholder.typicode.com/todos/" + currentTab)
+//       .then(async res => {
+//         const json = await res.json();
+//         setTabData(json);
+//         setLoading(false);
+//       });
+
+//   }, [])
+
+//   return <div>
+//     <button onClick={function() {
+//       setCurrentTab(1)
+//     }} style={{color: currentTab == 1 ? "red" : "black"}}>Todo #1</button>
+//     <button onClick={function() {
+//       setCurrentTab(2)
+//     }} style={{color: currentTab == 2 ? "red" : "black"}}>Todo #2</button>
+//     <button onClick={function() {
+//       setCurrentTab(3)
+//     }} style={{color: currentTab == 3 ? "red" : "black"}}>Todo #3</button>
+//     <button onClick={function() {
+//       setCurrentTab(4)
+//     }} style={{color: currentTab == 4 ? "red" : "black"}}>Todo #4</button>
+// <br />
+//     {loading ? "Loading..." : tabData.title}
+//   </div>
+// }
+
+// export default App
+
+import { useEffect, useState } from "react";
 
 function App() {
-  const [posts, setPosts] = useState([]);
+  const [currentTab, setCurrentTab] = useState(1);
+  const [data, setdata] = useState({});
+  const [loader, setLoder] = useState(true);
 
-  const postComponents = posts.map((post) => (
-    <PostComponent
-      name={post.name}
-      subtitle={post.subtitle}
-      time={post.title}
-      image={post.image}
-      description={post.description}
-    />
-  ));
-
-  function addPost() {
-    setPosts([
-      ...posts,
-      {
-        name: "harkirat",
-        subtitle: "10000 followers",
-        time: "2m ago",
-        image:
-          "https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
-        description:
-          "What to know how to win big? Check out how these folks won $6000 in bounties.",
-      },
-    ]);
-  }
-
+  useEffect( () => {
+    setLoder(true);
+    async function Fetchdata() {
+      const data = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/" + currentTab
+      );
+      const json = await data.json();
+      setdata(json);
+      setLoder(false);
+    }
+    Fetchdata();
+  }, [currentTab]);
   return (
-    <div style={{ background: "#dfe6e9", height: "100vh" }}>
-      <button onClick={addPost}>Add post</button>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <div>{postComponents}</div>
-      </div>
-    </div>
+    <>
+      <Button currentTab={currentTab} setCurrentTab={setCurrentTab}></Button>
+      <div>{loader ? "...loading" : `${JSON.stringify(data)}`}</div>
+    </>
+  );
+}
+
+function Button(props) {
+  return (
+    <>
+      <button
+        onClick={() => props.setCurrentTab(1)}
+        style={{ color: props.currentTab == 1 ? "red" : "black" }}
+      >
+        todo#1
+      </button>
+      <button
+        onClick={() => props.setCurrentTab(2)}
+        style={{ color: props.currentTab == 2 ? "red" : "black" }}
+      >
+        todo#2
+      </button>
+      <button
+        onClick={() => props.setCurrentTab(3)}
+        style={{ color: props.currentTab == 3 ? "red" : "black" }}
+      >
+        todo#3
+      </button>
+    </>
   );
 }
 
