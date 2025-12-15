95f1aff205a08d61f7c0db2903b232a58c53a01b
Apple <apple@Apples-MacBook-Air.local>
2025-12-15 10:33:49 +0530

    7th commit and learning the useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-import React, { useState } from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import { Landing } from "../component/landing";
-import { Lacation } from "../component/lacation";
-import { Language } from "../component/language";
-import { Name } from "../component/name";
-import { Layout } from "../component/outlet";
+import React, { useRef } from "react";
 
 function App() {
+  const reference = useRef(null);
+
   return (
     <>
-      <BrowserRouter>
-        <Link to="/">hello !</Link>|<Link to="/location">lacation</Link>|
-        <Link to="/language">Language</Link>
-        <br />
-        <br />
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Name />}></Route>
-            <Route path="/location" element={<Lacation />}></Route>
-            <Route path="/language" element={<Language />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <input
+        ref={reference}
+        type="text"
+        placeholder="write what yu want "
+      ></input>
+      <button
+        onClick={() => {
+          reference.current.focus();
+        }}
+      >click</button>
     </>
   );
 }
